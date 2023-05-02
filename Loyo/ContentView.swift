3c1af3fe0b2ae02c354f82eef8333dc4f7174fd4
luaroncrew <ma-kir.ru@yandex.ru>
2023-05-02 14:40:48 +0200

    Initial Commit

---
+++ Loyo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loyo
+//
+//  Created by Kirill on 5/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Loyo/LoyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoyoApp.swift
+//  Loyo
+//
+//  Created by Kirill on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LoyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
