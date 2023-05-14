78307e74f6af8e4cb144055b37089b79d4ebf15b
Nikita TEREKHOV <nikita.a.terekhov@gmail.com>
2023-05-14 23:57:31 +0200

    feat: setting up a relayer server

---
+++ relayer/src/transactions.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { TransactionsController } from './transactions.controller';
+
+@Module({
+  providers: [TransactionsService],
+  controllers: [TransactionsController],
+})
+export class TransactionsModule {}
